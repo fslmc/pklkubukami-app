14d254dbcf260b5a87a4541ea13f77e332a20726
fslmc <fslfaisal17@gmail.com>
2024-08-14 14:18:56 +0700

    hapus hapus

--- config/adminlte.php
+++ config/adminlte.php
@@ -470,7 +470,7 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',

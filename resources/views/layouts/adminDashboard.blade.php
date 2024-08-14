e435dad2ae4ef3abea14b834a2dd5eea18c0dcde
fslmc <fslfaisal17@gmail.com>
2024-08-14 14:29:19 +0700

    ganti gref jadi placeholder

--- resources/views/layouts/adminDashboard.blade.php
+++ resources/views/layouts/adminDashboard.blade.php
@@ -170,19 +170,19 @@
                   </a>
                   <ul class="nav nav-treeview">
                     <li class="nav-item">
-                      <a href="./index.html" class="nav-link active">
+                      <a href="#" class="nav-link active">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Dashboard v1</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="./index2.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Dashboard v2</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="./index3.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Dashboard v3</p>
                       </a>
@@ -200,25 +200,25 @@
                   </a>
                   <ul class="nav nav-treeview">
                     <li class="nav-item">
-                      <a href="pages/forms/general.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>General Elements</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="pages/forms/advanced.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Advanced Elements</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="pages/forms/editors.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Editors</p>
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="pages/forms/validation.html" class="nav-link">
+                      <a href="#" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Validation</p>
                       </a>
